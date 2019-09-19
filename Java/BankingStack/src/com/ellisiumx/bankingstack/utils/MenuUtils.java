91d4ed6689dd774b4c52656ac2b802755e882b87
João Pedro <joaopdante@gmail.com>
2019-09-19 14:19:38 -0300

    Remove color, fix blank space

--- Java/BankingStack/src/com/ellisiumx/bankingstack/utils/MenuUtils.java
+++ Java/BankingStack/src/com/ellisiumx/bankingstack/utils/MenuUtils.java
@@ -9,17 +9,6 @@ public class MenuUtils {
     public static final char FootLeftChar   = '╚';
     public static final char FootRightChar  = '╝';
 
-    // Usando ID's de cores iguais ao minecraft &1 &2...
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
     public static String getWindow(String[] content) { return getWindow("", content, ""); }
 
     public static String getWindow(String headTitle, String[] content) { return getWindow(headTitle, content, ""); }
@@ -55,44 +44,48 @@ public class MenuUtils {
         if(footTitle.length() > largestLength) largestLength = footTitle.length();
         for (String s : content) if(s.length() > largestLength) largestLength = s.length();
         largestLength += 2;
-        System.out.println(HeadLeftChar + parseToColor(getHead(headTitle, largestLength)) + HeadRightChar);
+        System.out.println(HeadLeftChar + getHead(headTitle, largestLength) + HeadRightChar);
         System.out.println(getContent(content, largestLength));
-        System.out.println(FootLeftChar + parseToColor(getFoot(footTitle, largestLength)) + FootRightChar);
+        System.out.println(FootLeftChar + getFoot(footTitle, largestLength) + FootRightChar);
     }
 
     public static String getHead(String title, int size) {
         String head;
         if(title.length() != 0) {
             head = " " + title + " ";
-            head = stringDirection(StringDirection.Center, head, size, stringSizeDifferenceColor(head), HorizontalChar);
+            head = stringDirection(StringDirection.Center, head, size, 0, HorizontalChar);
         }
-        else head = stringDirection(StringDirection.Center, "", size, stringSizeDifferenceColor(title), HorizontalChar);
+        else head = stringDirection(StringDirection.Center, "", size, 0, HorizontalChar);
         return head;
     }
 
     public static String getContent(String[] content, int size) {
         StringBuilder contentString = new StringBuilder();
+        int currentLine = 0;
         for(String data : content) {
             if(data.length() >= 2 && data.charAt(0) == '#') {
                 String directedString = data.substring(2);
                 switch (data.charAt(1)) {
                     case 'l':
                     case 'L':
-                        directedString = stringDirection(StringDirection.Left, directedString, size - 2, stringSizeDifferenceColor(directedString),' ');
+                        directedString = stringDirection(StringDirection.Left, directedString, size - 2, 0,' ');
                         break;
                     case 'r':
                     case 'R':
-                        directedString = stringDirection(StringDirection.Right, directedString, size, stringSizeDifferenceColor(directedString),' ');
+                        directedString = stringDirection(StringDirection.Right, directedString, size, 0,' ');
                         break;
                     default:
-                        directedString = stringDirection(StringDirection.Center, directedString, size - 2, stringSizeDifferenceColor(directedString),' ');
+                        directedString = stringDirection(StringDirection.Center, directedString, size - 2, 0,' ');
                         break;
                 }
-                contentString.append(System.lineSeparator()).append(VerticalChar).append(" ").append(parseToColor(directedString)).append(" ").append(VerticalChar);
+                if(currentLine != 0) contentString.append(System.lineSeparator());
+                contentString.append(VerticalChar).append(" ").append(directedString).append(" ").append(VerticalChar);
             } else {
-                String centerContent = stringDirection(StringDirection.Center, data, size, stringSizeDifferenceColor(data), ' ');
-                contentString.append(System.lineSeparator()).append(VerticalChar).append(parseToColor(centerContent)).append(VerticalChar);
+                String centerContent = stringDirection(StringDirection.Center, data, size, 0, ' ');
+                if(currentLine != 0) contentString.append(System.lineSeparator());
+                contentString.append(VerticalChar).append(centerContent).append(VerticalChar);
             }
+            currentLine++;
         }
         return contentString.toString();
     }
@@ -101,9 +94,9 @@ public class MenuUtils {
         String foot;
         if(title.length() != 0) {
             foot = " " + title + " ";
-            foot = stringDirection(StringDirection.Center, foot, size, stringSizeDifferenceColor(foot), HorizontalChar);
+            foot = stringDirection(StringDirection.Center, foot, size, 0, HorizontalChar);
         }
-        else foot = stringDirection(StringDirection.Center, title, size, stringSizeDifferenceColor(title), HorizontalChar);
+        else foot = stringDirection(StringDirection.Center, title, size, 0, HorizontalChar);
         return foot;
     }
 
@@ -142,32 +135,5 @@ public class MenuUtils {
             default:
                 return s;
         }
-    }
-
-    public static String parseToColor(String data) {
-        data = data.replace("&1", ANSI_BLACK);
-        data = data.replace("&1", ANSI_BLUE);
-        data = data.replace("&2", ANSI_GREEN);
-        data = data.replace("&4", ANSI_RED);
-        data = data.replace("&5", ANSI_PURPLE);
-        data = data.replace("&e", ANSI_YELLOW);
-        data = data.replace("&b", ANSI_CYAN);
-        data = data.replace("&f", ANSI_WHITE);
-        data = data.replace("&r", ANSI_RESET);
-        return data;
-    }
-
-    public static int stringSizeDifferenceColor(String data) {
-        int oldValue = data.length();
-        data = data.replace("&1", "");
-        data = data.replace("&1", "");
-        data = data.replace("&2", "");
-        data = data.replace("&4", "");
-        data = data.replace("&5", "");
-        data = data.replace("&e", "");
-        data = data.replace("&b", "");
-        data = data.replace("&f", "");
-        data = data.replace("&r", "");
-        return oldValue - data.length();
     }
 }
